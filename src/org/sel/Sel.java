4ab71ab7d5a89f94ad8661a56085ea85a08d9b43
SB-18688 <sathish18688@gmail.com>
2021-10-23 12:21:53 +0530

    updated

--- src/org/sel/Sel.java
+++ src/org/sel/Sel.java
@@ -15,9 +15,13 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Sel {
+	private void con() {
+		System.out.println("1919");
+	}
+	static WebDriver driver;
 	public static void main(String[] args) throws AWTException {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Sathish Babu\\eclipse-workspace\\Selenium-New\\lib\\chromedriver.exe");
-		WebDriver driver=new ChromeDriver();
+		driver=new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.amazon.com/");
 		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("iphones X");
